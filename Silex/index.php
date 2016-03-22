3295c6c57469a332a4d7a130d9b9079ce625f1f8
andsf <f.s.7272.f@gmail.com>
2016-03-22 17:16:53 +0900

    add index.php

---
+++ Silex/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once './vendor/autoload.php';
+echo 'test';
+//$app = new Silex\Application();
+
+//require_once __DIR__.'dispatchar.php';
+//require_once 'router.php';
+
+//$app->run();
