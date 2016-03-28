f4e27c5ee6e8dbacd198f4f1371d18eff1972b0e
andsf <f.s.7272.f@gmail.com>
2016-03-28 12:42:11 +0900

    modify kadai.sql

--- BootCamp/mysqldump/kadai.sql
+++ BootCamp/mysqldump/kadai.sql
@@ -95,6 +95,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'root','admin@i-enter.co.jp');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
